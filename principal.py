b27ffa626486e8cf59e6c304d34ff3c5957c7fef
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-10-06 20:08:39 -0300

    error6

--- principal.py
+++ principal.py
@@ -4,6 +4,8 @@ if 'flag' not in st.session_state:
   lista = []
 valor = st.number_input("Digite um numero")
 if st.button("Inserir"):
+  lista = st.session_state['lista']#lendo a lista
   st.session_state['flag'] = False
   lista.append(valor)
+  st.session_state['lista'] = lista #guardando a lista
 st.write(lista)
