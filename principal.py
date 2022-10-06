c57ec250c95c98d51b258d25e556f59d9888e1dc
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-10-06 20:06:22 -0300

    error2

--- principal.py
+++ principal.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-flag
-if flag != False:
+if 'flag' not in st.session_state:
   lista = []
 valor = st.number_input("Digite um numero")
 if st.button("Inserir"):
-  flag = False
+  st.session_state['flag'] = False
   lista.append(valor)
 st.write(lista)
